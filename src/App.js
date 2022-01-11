6735eb7ce8b10a10765d04ecb2a461fb0c5f3461
MorenaNobre <morenagnobre@gmail.com>
2022-01-11 17:09:20 -0300

    added wave

--- src/App.js
+++ src/App.js
@@ -7,13 +7,25 @@ import "./App.css";
 
 const WaveShaderMaterial = shaderMaterial(
   //Uniform
-  { uColor: new THREE.Color(0.0, 0.0, 0.0) },
+  { uTime: 0, uColor: new THREE.Color(0.0, 0.0, 0.0) },
   //Vertex Shader
   glsl`
+    // precision mediump float;
+
     varying vec2 vUv;
+
+    // uniform float uTime;
+
+    #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
 
     void main() {
       vUv = uv;
+
+      // vec3 pos = position;
+      // float noiseFreq = 1.5;
+      // float noiseAmp = 0.25;
+      // vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z)
+      // pos.z += snoise3(noisePos) * noiseAmp;
 
       gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
@@ -22,24 +34,35 @@ const WaveShaderMaterial = shaderMaterial(
   glsl`
     uniform vec3 uColor;
 
+    uniform float uTime;
+
     varying vec2 vUv;
 
     void main() {
-      gl_FragColor = vec4(vUv.x * uColor, 1.0);
+      gl_FragColor = vec4(sin(vUv.x + uTime) * uColor, 1.0);
     }
   `
 );
 
 extend({ WaveShaderMaterial });
 
+const Wave = () => {
+  const ref = useRef();
+  useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
+  return (
+    <mesh>
+      <planeBufferGeometry args={[0.4, 0.6, 16, 16]} />
+      <waveShaderMaterial uColor={"hotpink"} ref={ref} wireframe />
+    </mesh>
+  );
+};
+
 const Scene = () => {
   return (
-    <Canvas>
-      <pointLight position={[10, 10, 10]} />
-      <mesh>
-        <planeBufferGeometry args={[3, 5]} />
-        <waveShaderMaterial uColor={"hotpink"} />
-      </mesh>
+    <Canvas camera={{ fov: 10 }}>
+      <Suspense fallback={null}>
+        <Wave />
+      </Suspense>
     </Canvas>
   );
 };
