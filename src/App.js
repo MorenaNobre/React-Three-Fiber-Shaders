3e7af6fd762843f95d2556dd7f3780b1b8ba28b3
MorenaNobre <morenagnobre@gmail.com>
2022-01-11 15:02:28 -0300

    using vec for colors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import * as THREE from "three";
+import React, { useRef, Suspense } from "react";
+import { Canvas, extend, useFrame } from "@react-three/fiber";
+import { shaderMaterial } from "@react-three/drei";
+import glsl from "babel-plugin-glsl/macro";
+import "./App.css";
 
-function App() {
+const WaveShaderMaterial = shaderMaterial(
+  //Uniform
+  { uColor: new THREE.Color(0.0, 0.0, 0.0) },
+  //Vertex Shader
+  glsl`
+    varying vec2 vUv;
+
+    void main() {
+      vUv = uv;
+
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  //Fragment Shader
+  glsl`
+    uniform vec3 uColor;
+
+    varying vec2 vUv;
+
+    void main() {
+      gl_FragColor = vec4(vUv.x * uColor, 1.0);
+    }
+  `
+);
+
+extend({ WaveShaderMaterial });
+
+const Scene = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <pointLight position={[10, 10, 10]} />
+      <mesh>
+        <planeBufferGeometry args={[3, 5]} />
+        <waveShaderMaterial uColor={"hotpink"} />
+      </mesh>
+    </Canvas>
   );
-}
+};
+
+const App = () => {
+  return <Scene />;
+};
 
 export default App;
