9b9293f6018d2115435c9e5833494f51625c376b
MorenaNobre <morenagnobre@gmail.com>
2022-04-25 20:20:34 -0300

    README

---
+++ README.md
@@ -0,0 +1,66 @@
+<h1 align="center">Crazy Donuts - React ThreeJS</h1>
+
+[![Linkedin Badge](https://img.shields.io/badge/-MorenaNobre-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/morenanobre/)](https://www.linkedin.com/in/morenanobre/)
+[![Gmail Badge](https://img.shields.io/badge/-morenagnobre@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:morenagnobre@gmail.com)](mailto:morenagnobre@gmail.com)
+
+
+<blockquote align="center">“A chave para transformação é a continuidade. Continue a codar!!!”</blockquote>
+
+<br>
+
+<br>
+
+<hr>
+<br>
+
+## Sumário
+
+- [Apresentação](#apresentação)
+- [Sobre o Projeto](#sobre-o-projeto)
+- [Tecnologias utilizadas](#tecnologias-utilizadas)
+
+<br>
+
+# Apresentação
+
+<br>
+<div align="center">
+  <img alt="crazy donut" src="crazy-donuts.gif" width="100%">
+</div>
+<br>
+
+## Sobre o Projeto
+
+<br>
+
+<p>Tenho muito interesse em ThreeJS com React e assim sendo procurei um tutorial para treinar e entender um pouco mais essa tecnologia.</p>
+
+
+* ThreeJS;
+* ThreeJS Fiber;
+* ThreeJS Drei;
+* GLSL-Noise;
+* GLSLFY;
+
+Tutorial <a href="https://www.youtube.com/watch?v=kxXaIHi1j4w&t=1s" target="_blank">neste </a> vídeo.
+
+<br>
+
+
+## Tecnologias Utilizadas
+
+<br>
+
+<code><img height="32" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png" alt="react"/></code>
+<code><img height="32" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" alt="javascript"/></code>
+<code><img height="32" src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" alt="css"/></code>
+
+<br>
+<br>
+<hr>
+<br>
+<br>
+
+<h3 align="center">
+    <img alt="mayTheCodeBeWithYou" src="mayCode.png" height="80px" />
+</h3>
--- src/App.js
+++ src/App.js
@@ -1,45 +1,58 @@
 import * as THREE from "three";
 import React, { useRef, Suspense } from "react";
-import { Canvas, extend, useFrame } from "@react-three/fiber";
+import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber";
 import { shaderMaterial } from "@react-three/drei";
 import glsl from "babel-plugin-glsl/macro";
 import "./App.css";
 
 const WaveShaderMaterial = shaderMaterial(
   //Uniform
-  { uTime: 0, uColor: new THREE.Color(0.0, 0.0, 0.0) },
+  {
+    uTime: 0,
+    uColor: new THREE.Color(0.0, 0.0, 0.0),
+    uTexture: new THREE.Texture(),
+  },
   //Vertex Shader
   glsl`
-    // precision mediump float;
+    precision mediump float;
 
     varying vec2 vUv;
+    varying float vWave;
 
-    // uniform float uTime;
+    uniform float uTime;
 
     #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
+
+    // #pragma glslify: snoise3 = require(glsl-noise/simplex/3d);
 
     void main() {
       vUv = uv;
 
-      // vec3 pos = position;
-      // float noiseFreq = 1.5;
-      // float noiseAmp = 0.25;
-      // vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z)
-      // pos.z += snoise3(noisePos) * noiseAmp;
+      vec3 pos = position;
+      float noiseFreq = 2.5;
+      float noiseAmp = 0.55;
+      vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
+      pos.z += snoise3(noisePos) * noiseAmp;
+      vWave = pos.z;
 
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
     }
   `,
   //Fragment Shader
   glsl`
-    uniform vec3 uColor;
+    precision mediump float;
 
+    uniform vec3 uColor;
     uniform float uTime;
+    uniform sampler2D uTexture;
 
     varying vec2 vUv;
+    varying float vWave;
 
     void main() {
-      gl_FragColor = vec4(sin(vUv.x + uTime) * uColor, 1.0);
+      float wave = vWave * 0.2;
+      vec3 texture = texture2D(uTexture, vUv + wave).rgb;
+      gl_FragColor = vec4(texture, 1.0);
     }
   `
 );
@@ -49,17 +62,24 @@ extend({ WaveShaderMaterial });
 const Wave = () => {
   const ref = useRef();
   useFrame(({ clock }) => (ref.current.uTime = clock.getElapsedTime()));
+
+  const [image] = useLoader(THREE.TextureLoader, [
+    "https://images.unsplash.com/photo-1533910534207-90f31029a78e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  ]);
+
+  // const [image] = useLoader(THREE.TextureLoader, ["../../public/image/moloko-juice-bgOff.png"]);
+
   return (
     <mesh>
       <planeBufferGeometry args={[0.4, 0.6, 16, 16]} />
-      <waveShaderMaterial uColor={"hotpink"} ref={ref} wireframe />
+      <waveShaderMaterial uColor={"hotpink"} ref={ref} uTexture={image} />
     </mesh>
   );
 };
 
 const Scene = () => {
   return (
-    <Canvas camera={{ fov: 10 }}>
+    <Canvas camera={{ fov: 10, position: [0, 0, 5] }}>
       <Suspense fallback={null}>
         <Wave />
       </Suspense>
@@ -68,7 +88,12 @@ const Scene = () => {
 };
 
 const App = () => {
-  return <Scene />;
+  return (
+    <>
+      <h1>Crazy Donuts!</h1>
+      <Scene />
+    </>
+  );
 };
 
 export default App;
